479324b5ac90f65d4a7dbdea8cf471c51c712f9d
Chartok <m.bhimjee@outlook.com>
2023-08-23 16:12:41 -0500

    implementing mui styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
-import './Global.scss'
-import React from 'react';
+import * as React from 'react';
+import CssBaseLine from '@mui/material/CssBaseline'
+import theme from './theme.js'
+import { ThemeProvider } from '@mui/material/styles';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import About from './pages/About';
@@ -9,18 +11,21 @@ import Projects from './pages/Projects';
 
 export default function App() {
   return (
-    <Router>
-      <div className='App'>
-        <Header />
-        <main>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/projects' element={<Projects />} />
-            <Route path='/contact' element={<Contact />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <CssBaseLine />
+      <Router>
+        <div className='App'>
+          <Header />
+          <main>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/projects' element={<Projects />} />
+              <Route path='/contact' element={<Contact />} />
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    </ThemeProvider>
   );
 }
